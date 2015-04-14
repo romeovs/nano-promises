e7b173d757deab56982ddf1b48c0c51e35de414b
Romeo Van Snick <romeovs@gmail.com>
2015-04-14 23:42:45 +0200

    add initial test

--- test/instrument.js
+++ test/instrument.js
@@ -1,5 +1,40 @@
+// set up chai
 import chai from 'chai';
+import cas  from 'chai-as-promised';
+
+chai.use(cas);
+
+// set up db
+import nano from 'nano';
+import prom from '../src';
+
+var url    = 'http://localhost:5984/';
+var dbName = 'test';
+
+var couch = prom(nano(url));
+var db    = couch.db.use('test');
+
+var reset = function(done) {
+  couch.db.destroy(dbName)
+    .then(function(res) {
+      // db was deleted
+      return couch.db.create(dbName);
+    }, function(err) {
+      // db wasn't there in the first place
+      return couch.db.create(dbName);
+    })
+    .then(function(res) {
+      done();
+    })
+    .catch(function(err) {
+      done(err);
+    });
+};
+
 
 export default {
   expect: chai.expect
+, db
+, reset
 };
+
--- test/test.js
+++ test/test.js
@@ -1,12 +1,29 @@
-import { expect } from './instrument'
+import { db, expect, reset } from './instrument'
 import Lab from 'lab'
 
 var lab = Lab.script();
 var { describe, it } = lab;
 export { lab };
 
-// describe('a test', function() {
-//   it('should do this', function(done) {
-//
-//   });
-// });
+var id    = 'baz';
+var dummy = { foo: 'bar' };
+
+describe('get', function() {
+  lab.before(reset);
+
+  it('should insert one item', function(done) {
+    var insertion = db.insert(dummy, id);
+
+    expect(insertion).to
+      .not.be.rejected
+      .and.notify(done);
+  });
+
+  it('should be able to fetch one item', function(done) {
+    var lookup = db.get(id);
+
+    expect(lookup).to
+      .not.be.rejected
+      .and.notify(done);
+  });
+});
